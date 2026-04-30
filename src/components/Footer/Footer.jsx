7997646944bfed20a93d16afc9e6e2d273c7261f
Facundo <facundoanzoategui@outlook.com>
2026-04-30 19:21:54 -0300

    se linkeo el footer y se le dio estilo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,24 @@ export const Footer = () => {
     <footer>
       <nav>
         <ul className="nav-list">
-          <li>Whatsapp</li>
-          <li>Instagram</li>
+          <li>
+            <a 
+              href="https://wa.me/5491157432611" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              Whatsapp
+            </a>
+          </li>
+          <li>
+            <a 
+              href="https://www.instagram.com/facuanzo" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </li>
         </ul>
       </nav>
       <p className="copy-right">&copy; {year} My App. All rights reserved.</p>
